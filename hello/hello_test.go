ba68161f127a80ec9c5063b012d06a31747f8167
Victor Guadarrama <victor.guadarrama@infosel.com>
2025-05-09 14:07:43 -0600

    Refactor tests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Victor")
-	want := "Hello, Victor"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Victor")
+		want := "Hello, Victor"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("soy 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// tell Go this is a helper function. If a test fails, the line number reported will be in our function call
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
